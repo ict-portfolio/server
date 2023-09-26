115cc2000da79476b3128ab10498ada04d3e6904
Axel Philip <axel8102005@gmail.com>
2023-09-26 12:52:06 +0630

    first commit

---
+++ app/Http/Controllers/Admin/ContentController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Content;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ContentRequest;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\ResponseController;
+use App\Http\Resources\ContentResource;
+
+class ContentController extends ResponseController
+{
+    public function index()
+    {
+        return $this->success(ContentResource::collection(Content::with(['category','image'])->get()),"content datas");
+    }
+    public function store(ContentRequest $req)
+    {
+        $content = Request(["name","category_id","tags","image_id","description","status","paragraph"]);
+        $content['slug'] = Str::slug(Request()->name);
+        $data = new Content($content);
+        $data->save();
+        return $this->success($data,"Successfully created the {$data->name}",201);
+    }
+    public function show($slug)
+    {
+        $data = Content::where('slug',$slug)->with(['category','image'])->first();
+        if($data) {
+            return $this->success(new ContentResource($data),"detail of {$data->name}");
+        }else {
+            return $this->fail(["message" => "content doesn't exist",],"Not Found",404);
+        }
+    }
+    public function update(ContentRequest $req, $slug)
+    {
+        $data = Content::where('slug',$slug)->first();
+        if($data) {
+            $content = Request(["name","category_id","tags","image_id","description","status","paragraph"]);
+            $content['slug'] = Str::slug(Request()->name);
+            $data->update($content);
+            return $this->success($data, "Successfully updated the {$data->name}");
+        }else {
+            return $this->fail(["message" => "content doesn't exist",],"Not Found",404);
+        }
+    }
+}
---
+++ app/Http/Controllers/ResponseController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ResponseController extends Controller
+{
+    public function success($data,$message,$code = 200)
+    {
+        return $this->ApiResponse($data,[],$message,true,$code);
+    }
+    public function fail($error,$message,$code)
+    {
+        return $this->ApiResponse([],$error,$message,false,$code);
+    }
+    public function ApiResponse($data,$error,$message,$condition,$code)
+    {
+        return response()->json([
+            "data" => $data,
+            "error" => $error,
+            "message" => $message,
+            "condition" => $condition
+        ],$code);
+    }
+}
