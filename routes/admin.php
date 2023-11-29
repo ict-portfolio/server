61f7db93c61d2239764fb92722f82c8c506fac20
vadsha <heinzwe2626@gmail.com>
2023-11-29 13:22:53 +0630

    roadmap crud

--- routes/admin.php
+++ routes/admin.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\Admin\ContactController;
 use App\Http\Controllers\Admin\ContentController;
 use App\Http\Controllers\Admin\ServiceController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\RoadmapController;
 
 Route::get('/users' , [UserController::class , 'index']);
 Route::get('/make-admin/{id}' , [UserController::class , 'makeAdmin']);
@@ -22,6 +23,7 @@ Route::apiResource('contents',ContentController::class);
 Route::apiResource('services',ServiceController::class);
 Route::apiResource('sliders',SliderController::class);
 Route::apiResource('contacts' , ContactController::class);
+Route::apiResource('roadmap' , RoadmapController::class);
 Route::get('/fake-datas', function () {
     Category::factory()->count(5)->create();
     Content::factory()->count(15)->create();
